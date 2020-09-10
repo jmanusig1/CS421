32f6614bf6464a77002035b8bbdb3c2f913156b5
jmanusig1 <jmanus2@uic.edu>
2020-09-10 14:18:05 -0500

    word digits once again.,. still confused

--- q1.py
+++ q1.py
@@ -59,8 +59,8 @@ def alpha_ab():
 # Q1(e): the set of all strings that start with a word and that end with one or more digits
 # Return: regex as a valid python string
 def word_digits():   #TODO
-	#please....
-	return r"^(\s*\b[a-zA-Z]+)\s+([0-9])+\s*$"
+	#gambare!
+	return r"^(\s*\b[a-zA-Z]+)\s+([0-9])+$"
 
 
 # Q1(f): the set of all strings that have the exact words 'corona' and 'virus' in them
